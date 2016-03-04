b447d14ac91e3e165444f1ad9b9742639ba24a34
Collin Berg <berg.collin@gmail.com>
2016-03-04 11:39:01 -0600

    Updated to Latest Modernizr

--- lib/station.php
+++ lib/station.php
@@ -2,50 +2,6 @@
 /**
  * Theme Custom functions
  */
-// add_action('admin_init', 'shop_options');
-/*
-function shop_options() {
-
-	add_settings_section(
-		'bitbrilliant_options',
-		'Activate Comments',
-		'toggle_comments_callback',
-		'discussion'
-		);
-	
-	 add_settings_field( 
-        'show_comments',                     // ID
-        'Comments',                          // Label
-        'bit_comment_callback',  			 // The name of the function responsible for rendering the option interface
-        'discussion',                        // The page on which this option will be displayed
-        'bitbrilliant_options',         	 // The name of the section to which this field belongs
-        array(                               // The array of arguments to pass to the callback. In this case, just a description.
-            'Activate this setting to display the comments.'
-        )
-    );
-    register_setting(
-        'discussion',
-        'show_comments'
-    );
-}
-*/
-/** --------------------------------------------------
-Callbacks
--------------------------------------------------------**/
-/*
-function bit_comment_callback($args) {
-	 // Note the ID and the name attribute of the element match that of the ID in the call to add_settings_field
-    $html = '<input type="checkbox" id="show_comments" name="show_comments" value="1" ' . checked(1, get_option('show_comments'), false) . '/>'; 
-      
-    $html .= '<label for="show_comments"> '  . $args[0] . '</label>'; 
-    echo $html;
-}
-function toggle_comments_callback() {
-		echo '<p>Select which areas of content you wish to change.</p>';
-		
-	}
-*/
-	
 	
 
 /* ------------------------------------------------------------------------ *
