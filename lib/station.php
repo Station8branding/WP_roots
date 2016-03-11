acf0cbbf4752a8b508f342c0185f1fc28e06a60c
Collin Berg <berg.collin@gmail.com>
2016-03-11 14:39:36 -0600

    Added the widget Feed

--- lib/station.php
+++ lib/station.php
@@ -36,3 +36,53 @@ function is_tree($pid) {      // $pid = The ID of the page we're looking for pag
 	else 
                return false;  // we're elsewhere
 };
+/* ------------------------------------------------------------------------ *
+* Station8
+* Blog Feed
+ * ------------------------------------------------------------------------ */
+
+function s8_dashboard_widget_function() {
+    $rss = fetch_feed( "http://station8branding.com/feed/" );
+
+    if ( is_wp_error($rss) ) {
+        if ( is_admin() || current_user_can('manage_options') ) {
+            echo '<p>';
+            printf(__('<strong>RSS Error</strong>: %s'), $rss->get_error_message());
+            echo '</p>';
+        }
+        return;
+    }
+
+    if ( !$rss->get_item_quantity() ) {
+        echo '<p>Apparently, there are no updates to show!</p>';
+        $rss->__destruct();
+        unset($rss);
+        return;
+    }
+	echo "<header style='background: black; padding: 10px; text-align: center; margin: -11px -12px;'><a href='http://station8branding.com' target='_blank'><img src='http://station8branding.com/wp-content/uploads/2014/12/branding2.png'></a></header>";
+    echo "<ul>\n";
+
+    if ( !isset($items) )
+        $items = 5;
+
+    foreach ( $rss->get_items(0, $items) as $item ) {
+
+        $link = esc_url( strip_tags( $item->get_link() ) );
+        $title = esc_html( $item->get_title() );
+        $content = $item->get_content();
+        $content = wp_html_excerpt($content, 150) . ' ...';
+        //$image = $item->get_the_post_thumbnail();
+
+        echo "<li><a class='rsswidget' href='$link'>$title</a>\n<div class='rssSummary'>$content</div>\n";
+    }
+
+    echo "</ul>\n";
+    echo "<a href='http://station8branding.com'>Visit Station8 for more information</a>";
+    $rss->__destruct();
+    unset($rss);
+}
+
+function s8_add_dashboard_widget() {
+    wp_add_dashboard_widget('s8_dashboard_widget', 'Recent Posts from Station8 Branding', 's8_dashboard_widget_function');
+}
+add_action('wp_dashboard_setup', 's8_add_dashboard_widget');
